e0dda76eaa9da7c28a82a6d75e28e5cc5e9e235d
Josué Feitosa <josue.feitosa@outlook.com.br>
2020-06-01 15:03:45 -0300

    Implementando

---
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
+const appointmentsRouter = Router();
+appointmentsRouter.use(ensureAuthenticated);
+
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
+
+//   return response.json(appointments);
+// });
+
+appointmentsRouter.post('/', async (request, response) => {
+  const { date } = request.body;
+  const { id } = request.user;
+  const appointmentsRepository = new AppointmentsRepository();
+
+  const parsedDate = parseISO(date);
+
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
+
+  const appointment = await createAppointment.execute({
+    provider_id: id,
+    date: parsedDate,
+  });
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
