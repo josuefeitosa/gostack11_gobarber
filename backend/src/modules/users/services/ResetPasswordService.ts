19ea918b10ce9747f2a7f03f23dcd25afa321021
Josue Feitosa <josue.feitosa@outlook.com.br>
2020-08-20 09:05:46 -0300

    Implementação ResetPassword first test

---
+++ backend/src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,39 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+// import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  token: string;
+  newPassword: string;
+}
+
+@injectable()
+class ResetPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRespoitory')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ token, newPassword }: IRequest): Promise<void> {
+    const userToken = await this.userTokensRepository.findByToken(token);
+
+    if (!userToken) throw new AppError('User token does not exists.');
+
+    const user = await this.usersRepository.findById(userToken.user_id);
+
+    if (!user) throw new AppError('User does not exists.');
+
+    user.password = newPassword;
+
+    await this.usersRepository.save(user);
+  }
+}
+
+export default ResetPasswordService;
