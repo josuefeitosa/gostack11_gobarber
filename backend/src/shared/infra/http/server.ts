7e5d3e97ef6509a0f55a84bd6a47a33ebc1f749e
Josue Feitosa <josue.feitosa@outlook.com.br>
2020-09-09 15:34:48 -0300

    Validação das rotas

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 
@@ -23,6 +24,8 @@ app.use('/files', express.static(uploadConfig.uploadsFolder));
 
 // Using routes.ts
 app.use(routes);
+
+app.use(errors());
 
 // Global Exception Handling
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
