e23671b0bb79344fdde120dbc861169363e56e5a
Josue Feitosa <josue.feitosa@outlook.com.br>
2020-09-24 17:55:27 -0300

    Ajustes finais do back-end

--- backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -43,9 +44,7 @@ export default class ListProviderAppointmentsService {
         },
       );
 
-      console.log('Consultou banco');
-
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
